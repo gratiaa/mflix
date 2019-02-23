1e939fa110eb188c8bad6e589ad3927c6aa02d00
gratiaa <younggratia@gmail.com>
2019-02-23 23:07:30 +0900

    ticket: get comments

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -203,7 +203,7 @@ export default class MoviesDAO {
       sortStage,
       skipStage,
       limitStage,
-      facetStage
+      facetStage,
     ]
 
     try {
@@ -307,6 +307,25 @@ export default class MoviesDAO {
             _id: ObjectId(id),
           },
         },
+        {
+          $lookup: {
+            from: "comments",
+            let: { id: "$_id" },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ["$movie_id", "$$id"],
+                  },
+                },
+              },
+              {
+                $sort: { "date": -1 }
+              }
+            ],
+            as: "comments",
+          },
+        }
       ]
       return await movies.aggregate(pipeline).next()
     } catch (e) {
